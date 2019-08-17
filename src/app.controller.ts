a5f2781c037a7092122c2f4288062bb5206ccfb5
Alexander <arv.sander9@gmail.com>
2019-08-17 05:37:50 -0500

    Class-Validator basic example

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import {Body, Controller, Get, Post} from '@nestjs/common';
+import { AppService } from './app.service';
+import {CreateUserDto} from "./CreateUserDto";
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  
+  @Post()
+  create(@Body() createUserDto: CreateUserDto){
+    return 'This action adds a new user';
+  }
+  
+  
+}
